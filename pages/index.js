cf4c9b9791fa7ac0d0122bb955314544710dc422
Ariel <afbweb@hotmail.com>
2022-04-13 10:04:26 -0300

    add Helmet

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,34 @@ import Quiensoy from "./quiensoy";
 import Proyectos from "./proyectos";
 import Contacto from "./contacto";
 
+import { HelmetProvider } from "react-helmet-async";
+
 export default function Home() {
   return (
+    
     <>
+    <HelmetProvider>
+        <meta charSet="utf-8" />
+        <meta property='og:image' content='/images/screen-site.png'/>
+        <meta property='og:description' content='Actualmente me encuentro trabajando en el sector energético. Mi objetivo es dedicarme plenamente al desarrollo web. En función de ese objetivo estoy trabajando en diferentes proyectos utilizando el stack MERN (Mongo + Express + React + Node). Estoy en constante aprendizaje realizando cursos e interiorizándome en nuevas tecnologías.'/>
+        <meta property='og:url' content='https://arielbidart.vercel.app/'/>
+        <meta property="og:title" content="Ariel Bidart - Portfolio"/>
+        <title>Ariel Bidart - Portfolio</title>
+      </HelmetProvider>
+      
       <div className="ancho px-2 md:w1-8/12 md:w2-8/12 mx-auto">
+      
         <Navbar />
 
         <Header />
+        
 
         <Quiensoy />
 
         <Proyectos />
 
         <Contacto />
+        
       </div>
     </>
   );
